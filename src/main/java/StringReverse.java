a46e6f409459114a005fd267e6726febd08968fb
phorjax <joseph.12082@gmail.com>
2023-07-06 17:10:41 +0000

    done

--- src/main/java/StringReverse.java
+++ src/main/java/StringReverse.java
@@ -13,6 +13,11 @@ public class StringReverse {
      * @return the reverse of str.
      */
     public String reverse(String str){
-        return null;
+        StringBuilder reversed = new StringBuilder();
+        String container = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed.append(str.charAt(i));
+        }
+        return reversed.toString();
     }
 }
